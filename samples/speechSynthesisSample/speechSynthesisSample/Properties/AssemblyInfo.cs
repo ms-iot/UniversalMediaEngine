4991bbcd4f0ef5be5da10ed4208c5c5e731558cc
Ben Thomas <bethoma@microsoft.com>
2015-11-13 14:10:15 -0800

    Universal MediaEngine Release 1.1.0.1
    Notes:
    	- Update company to Microsoft

--- samples/speechSynthesisSample/speechSynthesisSample/Properties/AssemblyInfo.cs
+++ samples/speechSynthesisSample/speechSynthesisSample/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("speechSynthesisSample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("speechSynthesisSample")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright Microsoft Corporation ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
