4d654c26a7d6e4789a5e97f95ca03d1d32ea1225
Paul DeCarlo <pdecarlo@microsoft.com>
2016-03-10 13:21:00 -0600

    Convert C++ Sample to C#

--- samples/basicMediaEngineSample/basicMediaEngineSampleCSharp/MainPage.xaml.cs
+++ samples/basicMediaEngineSample/basicMediaEngineSampleCSharp/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Maker.Media.UniversalMediaEngine;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +24,32 @@ namespace basicMediaEngineSampleCSharp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private MediaEngine m_mediaEngine = new MediaEngine();
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            m_mediaEngine.MediaStateChanged += OnMediaStateChanged;
+            await m_mediaEngine.InitializeAsync();
+        }
+
+        private async void OnMediaStateChanged(MediaState state)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => CurrentStatus.Text = state.ToString());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            m_mediaEngine.Play(UrlBox.Text);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            m_mediaEngine.Pause();
         }
     }
 }
